2c8be3d7003ee94fbb982199f70512b83860eedf
ok2dok2 <lkim.pcr@gmail.com>
2023-08-14 13:12:36 +0900

    トピックにyoutubeを埋め込み、イベントの開催地のリンク化

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -20,9 +20,26 @@ class TopicsController < ApplicationController
     @post = @topic.posts.build
   end
 
+  def edit
+    @topic = Topic.find(params[:id])
+  end
+
+  def update
+    @topic = Topic.find(params[:id])
+    if @topic.update(topic_params)
+      redirect_to topic_post_path
+    end
+  end
+
+  def destroy
+    @topic = Topic.find(params[:id])
+    @topic.destroy
+    redirect_to topics_path
+  end
+
   private
 
   def topic_params
-    params.require(:topic).permit(:name, :image)
+    params.require(:topic).permit(:name, :image, :youtube)
   end
 end
