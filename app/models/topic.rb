77d214fc2a332c9c24e6c74e1c8c6b35180195c8
ok2dok2 <lkim.pcr@gmail.com>
2023-08-17 22:29:41 +0900

    herokuデプロイ後の挙動確認→修正

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,7 +1,7 @@
 class Topic < ApplicationRecord
   has_many :posts, dependent: :destroy
   validates :name, presence: true
-  validates :name, length: { is: 11, allow_blank: true }
+  validates :youtube, length: { is: 11, allow_blank: true }
   validates :image, presence: true
   belongs_to :user
   mount_uploader :image, Image2Uploader
